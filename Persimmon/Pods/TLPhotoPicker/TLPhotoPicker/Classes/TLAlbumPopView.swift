073664eaa409e2bac17b0195c9ed9003d888cbb9
hyeoktae kwon <kht_0218@hanmail.net>
2020-06-13 00:49:44 +0900

    feat: tass - repo update

--- Persimmon/Pods/TLPhotoPicker/TLPhotoPicker/Classes/TLAlbumPopView.swift
+++ Persimmon/Pods/TLPhotoPicker/TLPhotoPicker/Classes/TLAlbumPopView.swift
@@ -25,8 +25,10 @@ extension PopupViewProtocol where Self: UIView {
         return frame
     }
     func setupPopupFrame() {
-        if self.originalFrame != self.popupView.frame {
+        if self.originalFrame == CGRect.zero {
             self.originalFrame = self.popupView.frame
+        }else {
+            self.originalFrame.size.height = self.popupView.frame.height
         }
     }
     func show(_ show: Bool, duration: TimeInterval = 0.1) {
@@ -52,7 +54,7 @@ extension PopupViewProtocol where Self: UIView {
     }
 }
 
-open class TLAlbumPopView: UIView,PopupViewProtocol {
+open class TLAlbumPopView: UIView, PopupViewProtocol {
     @IBOutlet open var bgView: UIView!
     @IBOutlet open var popupView: UIView!
     @IBOutlet var popupViewHeight: NSLayoutConstraint!
@@ -64,24 +66,15 @@ open class TLAlbumPopView: UIView,PopupViewProtocol {
 //        print("deinit TLAlbumPopView")
     }
     
-    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if #available(iOS 13.0, *) {
-            let userInterfaceStyle = self.traitCollection.userInterfaceStyle
-            if userInterfaceStyle.rawValue == 2 {
-                self.popupView.backgroundColor = .systemBackground
-            }else {
-                self.popupView.backgroundColor = .white
-            }
-        }
-    }
-    
     override open func awakeFromNib() {
         super.awakeFromNib()
         self.popupView.layer.cornerRadius = 5.0
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapBgView))
         self.bgView.addGestureRecognizer(tapGesture)
         self.tableView.register(UINib(nibName: "TLCollectionTableViewCell", bundle: TLBundle.bundle()), forCellReuseIdentifier: "TLCollectionTableViewCell")
+        if #available(iOS 13.0, *) {
+            self.popupView.backgroundColor = .systemBackground
+        }
     }
     
     @objc func tapBgView() {
